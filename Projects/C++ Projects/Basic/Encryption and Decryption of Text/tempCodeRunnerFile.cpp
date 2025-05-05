821df5459aa330a3d8d9b45cb4a7fe07648f3476
Simranjit kaur <amatjitsohal769@gmail.com>
2025-05-05 18:45:19 +0530

    Refactored encryption code and added input instructions

--- Projects/C++ Projects/Basic/Encryption and Decryption of Text/tempCodeRunnerFile.cpp
+++ Projects/C++ Projects/Basic/Encryption and Decryption of Text/tempCodeRunnerFile.cpp
@@ -4,21 +4,19 @@
 
 
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main()
 {
    int i, x;
-   string str;
+   char str[100];
 
-   cout << "Please enter a string to convert to a secret code: ";
+   cout << "Please enter a string:\t";
    cin >> str;
 
    cout << "\nPlease choose following options:\n";
    cout << "1 = Encrypt the string.\n";
    cout << "2 = Decrypt the string.\n";
-   cout<<"Enter option 1 or 2: ";
    cin >> x;
 
    //using switch case statements
@@ -26,22 +24,22 @@ int main()
    {
       //first case for encrypting a string
       case 1:
-         for(i = 0; i<str.length(); i++)
+         for(i = 0; (i < 100 && str[i] != '\0'); i++)
             str[i] = str[i] + 2; //the key for encryption is 3 that is added to ASCII value
 
-         cout << "\nEncrypted string: " << str << endl<<endl;
+         cout << "\nEncrypted string: " << str << endl;
          break;
 
       //second case for decrypting a string
       case 2:
-         for(i = 0; i < str.length(); i++)
+         for(i = 0; (i < 100 && str[i] != '\0'); i++)
          str[i] = str[i] - 2; //the key for encryption is 3 that is subtracted to ASCII value
 
-      cout << "\nDecrypted string: " << str << endl <<endl;
+      cout << "\nDecrypted string: " << str << endl;
       break;
 
       default:
-         cout << "\nInvalid Input.Enter a valid option!!!\n";
+         cout << "\nInvalid Input !!!\n";
    }
    return 0;
 }
